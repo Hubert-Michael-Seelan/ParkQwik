305c309e4396deac586b64ac312305b0dce786b2
Hubert_Michael_Seelan <hubertmichaelseelan@gmail.com>
2024-03-22 16:24:56 +0530

    update

--- Components/FlexExample.jsx
+++ Components/FlexExample.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { View, StyleSheet, SafeAreaView } from "react-native";
+import {
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Button,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import Profile from "./Profile";
 
 const FlexExample = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={[styles.box, { flex: 1 }]}>
-        <Profile />
+        <Profile imageSource="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" />
+        <TouchableOpacity style={styles.btn}>
+          <Text style={{color:"rgba(26, 158, 117, 1)"}}> + Add Vehicle</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
@@ -19,15 +29,25 @@ const styles = StyleSheet.create({
     // justifyContent: 'space-around', // Align boxes with equal space around them
     // alignItems: 'center', // Align boxes vertically at the center
   },
+  btn: {
+    width: 159,
+    height: 32,
+    backgroundColor:"rgba(240, 255, 250, 1)",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 84,
+  },
   box: {
     width: 400,
     height: 400,
+    flexDirection: "column",
     backgroundColor: "rgba(26,158,117,255)",
-    justifyContent: "center",
+    // justifyContent: "start",
     // alignItems: "center",
     // borderRadius: "50px",
-    borderBottomLeftRadius: 60, // Adjust the border radius to match the main box
-    borderBottomRightRadius: 60, // Adjust the border radius to match the main box
+    borderBottomLeftRadius: 100, // Adjust the border radius to match the main box
+    borderBottomRightRadius: 100, // Adjust the border radius to match the main box
   },
 });
 
--- Components/Profile.jsx
+++ Components/Profile.jsx
@@ -11,16 +11,23 @@ const Profile = ({ imageSource }) => {
 
 const styles = StyleSheet.create({
   container: {
-    width: 80, // Diameter of the circle
-    height: 80, // Diameter of the circle
+    width: 51, // Diameter of the circle
+    height: 51, // Diameter of the circle
     backgroundColor: "white",
+    display: "flex",
+    marginTop: 62,
+    marginLeft: 13,
     borderRadius: 50, // Half of the width/height to create a circle
     // overflow: 'hidden', // Ensures the image stays within the circle
   },
   image: {
-    width: '50%',
-    height: '50%',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 50,
     resizeMode: 'cover', // Ensure the image covers the entire circle
+
   },
 });
 
